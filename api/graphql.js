3a8b72eda5aa9bd1e610c057c6f8189b1bee6a7d
yuvrajrana <yuvraj_raj87@yahoo.co.in>
2025-12-14 20:40:59 +0530

    updated path

--- api/graphql.js
+++ api/graphql.js
@@ -1,15 +1,21 @@
 import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
-import { typeDefs } from "../../graphql/schema.js";
-import { resolvers } from "../../resolvers/resolvers.js";
+import { typeDefs } from "../graphql/schema.js";
+import { resolvers } from "../resolvers/resolvers.js";
 import mongoose from "mongoose";
 
+// Create Apollo Server
 const server = new ApolloServer({ typeDefs, resolvers });
 
+// Export serverless handler
 export default startServerAndCreateNextHandler(server, {
-    context: async (req, res) => {
+    context: async ({ req, res }) => {
+        // Connect to MongoDB only if not already connected
         if (mongoose.connection.readyState === 0) {
-            await mongoose.connect(process.env.MONGODB_URI);
+            await mongoose.connect(process.env.MONGODB_URI, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            });
         }
         return { req, res };
     },
